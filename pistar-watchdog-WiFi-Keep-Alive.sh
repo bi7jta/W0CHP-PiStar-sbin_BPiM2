da87ed274efef2d53171baf45fc14529d4a14878
BI7JTA <bi7jta@gmail.com>
2023-10-23 23:17:46 +0800

    Fix BPiM2 betty WiFi keep alive script.

--- pistar-watchdog-WiFi-Keep-Alive.sh
+++ pistar-watchdog-WiFi-Keep-Alive.sh
@@ -14,12 +14,35 @@ while true
 do
     CURTIME=`date "+%Y-%m-%d %H:%M:%S"`
     echo "$CURTIME: call WiFi auto Down watchdog, loop after 60s " >> /var/log/pi-star/pistar-watchdog-WiFi-Keep-Alive.log
+
+    
+    if [ -d "/sys/class/net/wlan0" ]; then	   
+	echo "DEBUG: check /sys/class/net/wlan0 exist " >> /var/log/pi-star/pistar-watchdog-WiFi-Keep-Alive.log
+	echo "DEBUG: cat /sys/class/net/wlan0/operstate  " >> /var/log/pi-star/pistar-watchdog-WiFi-Keep-Alive.log
+	cat /sys/class/net/wlan0/operstate >> /var/log/pi-star/pistar-watchdog-WiFi-Keep-Alive.log
+    else
+	echo "DEBUG: /sys/class/net/wlan0 NOT exist"  >> /var/log/pi-star/pistar-watchdog-WiFi-Keep-Alive.log
+    fi
+
+    if [ -d "/sys/class/net/wlan0_ap" ]; then
+	echo "DEBUG: check /sys/class/net/wlan0_ap exist"  >> /var/log/pi-star/pistar-watchdog-WiFi-Keep-Alive.log
+    else
+	echo "DEBUG: check /sys/class/net/wlan0_ap NOT exist"  >> /var/log/pi-star/pistar-watchdog-WiFi-Keep-Alive.log
+    fi
+
     #Solve BPiM2 betty  dhd_prot_ioctl : bus is down. we have nothing to do
     if [ -d "/sys/class/net/wlan0" ] && [ ! -d "/sys/class/net/wlan0_ap" ]; then
-      if [ `cat /sys/class/net/wlan0/operstate` == "down" ]; then
+        echo "DEBUG: /sys/class/net/wlan0 exist and /sys/class/net/wlan0_ap NOT exist"  >> /var/log/pi-star/pistar-watchdog-WiFi-Keep-Alive.log 
+        #if [ `cat /sys/class/net/wlan0/operstate` == "down" ]; then
+        #NOTE: If work in nohup and in /etc/rc.local call, can use this adjustment, it will never got "Down"
+        if [ `grep -c "down" /sys/class/net/wlan0/operstate` -ne '0' ];then
         echo "$CURTIME: find WLAN0 down and not in AP mode,  call ip link set wlan0 up " >> /var/log/pi-star/pistar-watchdog-WiFi-Keep-Alive.log
         ip link set wlan0 up
-      fi
+       else
+        echo "DEBUG: something check error, Code:002"  >> /var/log/pi-star/pistar-watchdog-WiFi-Keep-Alive.log 
+       fi
+    else
+       echo "DEBUG: something check error, Code:001"  >> /var/log/pi-star/pistar-watchdog-WiFi-Keep-Alive.log 
     fi
  sleep 60 
  
