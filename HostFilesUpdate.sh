c0bd6c7af963f03776412b35fdb0100473d66e57
BI7JTA <bi7jta@gmail.com>
2023-12-01 07:02:22 +0800

    FIX p25gateway no data, FIX: NO FLAG when the MW0MWZ version updte to this.

--- HostFilesUpdate.sh
+++ HostFilesUpdate.sh
@@ -48,6 +48,23 @@ else
 	NXDNHOSTS=/usr/local/etc/NXDNHosts.txt 
 	XLXHOSTS=/usr/local/etc/XLXHosts.txt 
 	M17HOSTS=/usr/local/etc/M17Hosts.txt
+	#W0CHP独有的本地文件定义
+	APRSHOSTS=/usr/local/etc/APRSHosts.txt
+	APRSSERVERS=/usr/local/etc/aprs_servers.json
+	NXDNIDFILE=/usr/local/etc/NXDN.csv 
+	TGLISTBM=/usr/local/etc/TGList_BM.txt
+	TGLISTTGIF=/usr/local/etc/TGList_TGIF.txt
+	TGLISTFREESTARIPSC2=/usr/local/etc/TGList_FreeStarIPSC.txt
+	TGLISTSYSTEMX=/usr/local/etc/TGList_SystemX.txt
+	TGLISTFREEDMR=/usr/local/etc/TGList_FreeDMR.txt
+	TGLISTDMRPLUS=/usr/local/etc/TGList_DMRp.txt
+	TGLISTP25=/usr/local/etc/TGList_P25.txt
+	TGLISTNXDN=/usr/local/etc/TGList_NXDN.txt
+	TGLISTYSF=/usr/local/etc/TGList_YSF.txt
+	BMTGNAMES=/usr/local/etc/BM_TGs.json
+	GROUPSTXT=/usr/local/etc/groups.txt
+	STRIPPED=/usr/local/etc/stripped.csv
+	COUNTRIES=/usr/local/etc/country.csv
 
 	# 默认情况下，curl是不会显示下载进度的。但是，你可以通过使用“-#”或“--progress-bar”选项来启用进度条 -s：静默不输出任何信息
 
@@ -58,11 +75,14 @@ else
 
     HostURL="http://www.pistar.uk/downloads"
     NextionHostURL="https://radioid.net/static"
+    W0CHP_hostFileURL="https://hostfiles.w0chp.net"
 
    file="/usr/local/sbin/.git/config"
    if [ -f "$file" ] && grep -q "gitee.com" "$file"; then
 		HostURL="https://www.bi7jta.cn/files/dmrids-and-hosts"
 		NextionHostURL=${HostURL}
+        W0CHP_hostFileURL=${HostURL}
+
 	    echo "Now in Chinse Repositories [gitee.com], HostURL change to ${HostURL}, NextionHostURL ${NextionHostURL} "
         #Not have blank from Agent,
 	    sudo curl -# -o ${P25HOSTS}  ${HostURL}/P25Hosts.txt --user-agent "Pi-Star_${pistarCurVersion}"
@@ -71,6 +91,8 @@ else
 	    sudo curl -# -o ${M17HOSTS}  ${HostURL}/M17Hosts.txt --user-agent "Pi-Star_${pistarCurVersion}"
 	else 
 	    echo "Now in Github Repositories, HostURL is ${HostURL} ,NextionHostURL ${NextionHostURL} "
+	    # W0CHP_hostFileURL="https://hostfiles.w0chp.net"
+
 	    #Have blank from pistar.uk,
 	    sudo curl -# -o ${P25HOSTS}  ${HostURL}/P25_Hosts.txt --user-agent "Pi-Star_${pistarCurVersion}"
 	    sudo curl -# -o ${YSFHOSTS}  ${HostURL}/YSF_Hosts.txt --user-agent "Pi-Star_${pistarCurVersion}"
@@ -80,6 +102,26 @@ else
 	
 	sudo curl -# -o ${DMRHOSTS}  ${HostURL}/DMR_Hosts.txt --user-agent "Pi-Star_${pistarCurVersion}"
 	sudo curl -# -o ${XLXHOSTS}  ${HostURL}/XLXHosts.txt --user-agent "Pi-Star_${pistarCurVersion}"
+
+	#W0CHP独有的
+	# #Generate Host Files
+	uaStr="WPSD-HostFileUpdater Ver.# 4.1.6 283205d9d2 (master) Call:BG6THE UUID:00000000130d8a21_MMDVM_stm32usb_Simplex [Raspberry Pi 3 Model B Plus Rev 1.3 - ARMv7 Processor rev 4 (v7l)] [buster]"
+
+	curl -# -o ${APRSHOSTS}  ${hostFileURL}/APRS_Hosts.txt --user-agent "${uaStr}"
+	curl -# -o ${APRSSERVERS}  ${hostFileURL}/aprs_servers.json --user-agent "${uaStr}"
+	curl -# -o ${TGLISTBM}  ${W0CHP_hostFileURL}/TGList_BM.txt --user-agent "${uaStr}"
+	curl -# -o ${TGLISTTGIF}  ${W0CHP_hostFileURL}/TGList_TGIF.txt --user-agent "${uaStr}"
+	curl -# -o ${TGLISTFREESTARIPSC2}  ${W0CHP_hostFileURL}/TGList_FreeStarIPSC.txt --user-agent "${uaStr}"
+	curl -# -o ${TGLISTSYSTEMX}  ${W0CHP_hostFileURL}/TGList_SystemX.txt --user-agent "${uaStr}"
+	curl -# -o ${TGLISTFREEDMR}  ${W0CHP_hostFileURL}/TGList_FreeDMR.txt --user-agent "${uaStr}"
+	curl -# -o ${TGLISTDMRPLUS}  ${W0CHP_hostFileURL}/TGList_DMRp.txt --user-agent "${uaStr}"
+	curl -# -o ${TGLISTP25}  ${W0CHP_hostFileURL}/TGList_P25.txt --user-agent "${uaStr}"
+	curl -# -o ${TGLISTNXDN}  ${W0CHP_hostFileURL}/TGList_NXDN.txt --user-agent "${uaStr}"
+	curl -# -o ${TGLISTYSF}  ${W0CHP_hostFileURL}/TGList_YSF.txt --user-agent "${uaStr}"
+	curl -# -o ${COUNTRIES}  ${W0CHP_hostFileURL}/country.csv --user-agent "${uaStr}"
+	curl -# -o ${BMTGNAMES}  ${W0CHP_hostFileURL}/BM_TGs.json --user-agent "${uaStr}"
+    #BM TG List for live caller and nextion screens:
+    cp ${BMTGNAMES} ${GROUPSTXT}
     
     #Update DMRids
     if [ -n "$1" ]; then 
@@ -89,13 +131,17 @@ else
     else
     	echo "Upate All including DMRids, Nextion DMRIds ..."
 	    sudo curl -# -o ${DMRIDFILE} ${HostURL}/DMRIds.dat --user-agent "Pi-Star_${pistarCurVersion}"
+	    
 	    echo "Update NextionDriver DMRIds from radioid.net/static/user.csv ... " 
 		cd /tmp; sudo rm -f user.*;  
 		sudo curl -# -o /tmp/user.csv  ${NextionHostURL}/user.csv --user-agent "Pi-Star_${pistarCurVersion}"
 		mv /tmp/user.csv /usr/local/etc/stripped.csv
 		stat /usr/local/etc/stripped.csv
-    fi
 
+        echo "Update NXDN.csv ... "
+		curl -# -o ${NXDNIDFILE}  ${W0CHP_hostFileURL}/NXDN.csv --user-agent "${uaStr}" 
+		stat ${NXDNIDFILE} 
+    fi
 
     # If there is a DMR override file, add its contents to DMR_Hosts.txt
 	if [ -f "/root/DMR_Hosts.txt" ]; then
